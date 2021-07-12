113f8afb0bc13eaa4448f7f933492d6bb21388c8
Silvia Garcia <garcia.s091@gmail.com>
2021-07-11 23:13:05 -0500

    App Productos

---
+++ 3-Axios/app-axios/src/components/AddProducto.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const AddProducto = () => {
+    return (
+        <div className="col-md-8 mx-auto">
+            <h1 className="text-center">Agregar Producto</h1>
+            <form
+                className="mt-5"
+            >
+                <div className="form-group">
+                    <label>Nombre Platillo</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="nombre"
+                        placeholder="Nombre Platillo"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Precio Platillo</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        name="precio"
+                        placeholder="Precio Platillo"
+                    />
+                </div>
+
+                <legend className="text-center">Categoría:</legend>
+                <div className="text-center">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="postre"
+
+                        />
+                        <label className="form-check-label">
+                            Postre
+                        </label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="bebida"
+
+                        />
+                        <label className="form-check-label">
+                            Bebida
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="carnes"
+
+                        />
+                        <label className="form-check-label">
+                            Carnes
+                        </label>
+                    </div>
+
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="categoria"
+                            value="ensalada"
+
+                        />
+                        <label className="form-check-label">
+                            Ensalada
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" className="font-weight-bold text-uppercase mt-5 btn btn-primary btn-block py-3" value="Agregar Producto" />
+            </form>
+        </div>
+    )
+}
+
+export default AddProducto
---
+++ 3-Axios/app-axios/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+                <div className="container-fluid">
+                    
+                    <Link className="navbar-brand" to="/">
+                        <img src="https://res.cloudinary.com/db9wh5uvt/image/upload/v1622495473/ncmfdru8urbqfxwyqkfc.png"
+                         alt="" width="50px"/>
+                         Guajalotas
+                    </Link>
+                    
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" aria-current="page" to="/nuevo-producto">Nuevo Producto</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/editar-producto">Editar Producto</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
